26c7cbcae9d7d372eae01a193b9abd7fc81ee039
Karl Moore <krm97@uclive.ac.nz>
2022-04-03 14:09:05 +1200

    Only images left now!

--- src/app/routes/auctions.routes.ts
+++ src/app/routes/auctions.routes.ts
@@ -7,9 +7,13 @@ module.exports = (app: Express) => {
     app.route(rootUrl + '/auctions')
         .post(auctions.create)
         .get(auctions.list)
+    app.route(rootUrl + '/auctions/categories')
+        .get(auctions.readCategories)
     app.route(rootUrl + '/auctions/:id/bids')
         .post(auctions.createBid)
         .get(auctions.readBids)
     app.route(rootUrl + '/auctions/:id')
         .get(auctions.read)
+        .delete(auctions.deleteAuction)
+        .patch(auctions.update)
 };
